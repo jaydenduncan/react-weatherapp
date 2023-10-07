be6c82a90618567f1bfda22952d4cb013137de33
Jayden <jduncan9@stu.jsu.edu>
2023-10-06 19:52:04 -0500

    Fetch weather data from an api

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -3,4 +3,4 @@ module.exports = {
     'license': 'free'
   }
 }
-  
+ 
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './WeatherApp.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
@@ -12,40 +12,113 @@ import stormy_icon from './images/stormy.png';
 import sunny_icon from './images/sunny.png';
 
 function WeatherApp() {
+    const Weather = {
+        CLEAR: "Clear",
+        CLOUDY: "Cloudy",
+        LIGHTNING: "Lightning",
+        PARTLY_CLOUDY: "Partly Cloudy",
+        RAINY: "Rain",
+        SNOWY: "Snow",
+        STORMY: "Storm",
+        SUNNY: "Sunny"
+    };
+
+    const [city, setCity] = useState("");
+    const [currentTemp, setCurrentTemp] = useState(0);
+    const [lowTemp, setLowTemp] = useState(0);
+    const [highTemp, setHighTemp] = useState(0);
+    const [icon, setIcon] = useState(sunny_icon);
+    const [iconDesc, setIconDesc] = useState(Weather.SUNNY);
+    const [windSpeed, setWindSpeed] = useState(0);
+    const [humidity, setHumidity] = useState(0);
+
+    // Define API settings
+    let options = {
+        method: 'GET',
+        headers: {'x-api-key':process.env.REACT_APP_API_KEY}
+    };
+
+    // Update screen to match new data
+    const updateScreen = (data, inputCity) => {
+        setCity(inputCity);
+        setCurrentTemp(data["temp"]);
+        setLowTemp(data["min_temp"]);
+        setHighTemp(data["max_temp"]);
+        setWindSpeed(Math.floor(data["wind_speed"]));
+        setHumidity(data["humidity"]);
+
+        document.getElementById("inputCity").value = "";
+    };
+
+    // Initialize screen with data
+    const initialize = () => {
+        fetch('https://api.api-ninjas.com/v1/weather?city=London', options)
+        .then(response => response.json())
+        .then(data => {
+            if(data["error"]){
+                alert('ERROR: Something went wrong');
+            }
+            else{
+                updateScreen(data, 'London');
+            }
+        })
+        .catch(err => console.log(`error ${err}`));
+    };
+
+    // Fetch weather data from API
+    const getWeatherInfo = () => {
+        let inputCity = document.getElementById("inputCity").value;
+
+        fetch(`https://api.api-ninjas.com/v1/weather?city=${inputCity}`, options)
+        .then(response => response.json())
+        .then(data => {
+            if(data["error"]){
+                alert('ERROR: Please enter a valid city.');
+            }
+            else{
+                updateScreen(data, inputCity);
+            }
+        })
+        .catch(err => console.log(`error ${err}`));
+    };
+
+    useEffect(() => {
+        initialize();
+    }, []);
 
     return (
         <div className="container">
             <p className="appHeading">React Weather App</p>
             <div className="searchSection">
-                <input type="text" placeholder="Search a city" />
-                <div className="searchBtn">
+                <input id="inputCity" type="text" placeholder="Search a city"/>
+                <div className="searchBtn" onClick={getWeatherInfo}>
                     <FontAwesomeIcon className="searchIcon" icon={faMagnifyingGlass} />
                 </div>
             </div>
-            <p className="cityName">Los Angeles</p>
-            <p className="currentTemp">28&deg;C</p>
+            <p className="cityName">{city}</p>
+            <p className="currentTemp">{currentTemp}&deg;C</p>
             <div className="lowhighHeading">
                 <p>Low</p>
                 <p>High</p>
             </div>
             <div className="lowhighTemps">
-                <p>23&deg;C</p>
+                <p>{lowTemp}&deg;C</p>
                 <p>/</p>
-                <p>36&deg;C</p>
+                <p>{highTemp}&deg;C</p>
             </div>
             <div className="weatherIconSpace">
-                <img className="weatherIcon" src={sunny_icon} alt="Sunny Icon" />
-                <p className="weatherIconDesc">Sunny</p>
+                <img className="weatherIcon" src={icon} alt="Sunny Icon" />
+                <p className="weatherIconDesc">{iconDesc}</p>
             </div>
             <div className="extraInfo">
                 <div className="windSpeed">
                     <p className="windSpeedHeading">Wind Speed</p>
-                    <p className="windSpeedVal">5</p>
+                    <p className="windSpeedVal">{windSpeed}</p>
                     <p>mph</p>
                 </div>
                 <div className="humidity">
                     <p className="humidityHeading">Humidity</p>
-                    <p className="humidityVal">37&#37;</p>
+                    <p className="humidityVal">{humidity}&#37;</p>
                 </div>
             </div>
         </div>
